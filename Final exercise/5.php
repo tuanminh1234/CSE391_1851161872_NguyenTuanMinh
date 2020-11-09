7b1e15795e9cb428b91af4f1ea2575e8dae7a9ee
tuanminh1234 <67354302+tuanminh1234@users.noreply.github.com>
2020-11-09 10:54:47 +0700

    update

---
+++ Final exercise/5.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Trường đại học kinh tế kỹ thuật công nghiệp</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="wrapper">
+            <img src="images/banner-phongdaotao.jpg" width="100%">
+
+            <div id="menu">
+                <nav class="navbar navbar-expand-sm navbar-dark bg-primary S">
+                    <a class="navbar-brand " href="#"></a>
+                    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php"><img src="images/home.png" alt="Trang chủ"></img> Trang chủ
+                                    <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="1.php">Giới thiệu</a>
+                            </li>
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Chương trình đào tạo</a>
+                                <div class="dropdown-menu color" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="http://daotao.uneti.edu.vn/PageContent.aspx?MenuID=376" style="color: whitesmoke;">Đại học chính quy</a>
+                                    <a class="dropdown-item" href="http://daotao.uneti.edu.vn/PageContent.aspx?MenuID=378" style="color: whitesmoke;">Cao đẳng chính quy</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Quy chế quy định</a>
+                                <div class="dropdown-menu color" aria-labelledby="dropdownId">
+                                    <a class="dropdown-item" href="2.php" style="color: whitesmoke;">Quy chế, quy định</a>
+                                    <a class="dropdown-item" href="3.php" style="color: whitesmoke;">Văn bản Luật</a>
+                                    <a class="dropdown-item" href="4.php" style="color: whitesmoke;">Chương trình giải quyết thủ tục hành chính</a>
+                                    <a class="dropdown-item" href="5.php" style="color: whitesmoke;">Biểu mẫu dùng cho HSSV</a>
+                                    <a class="dropdown-item" href="6.php" style="color: whitesmoke;">Quy chế của Bộ Giáo dục và Đào tạo</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://tuyensinh.uneti.edu.vn/">Tuyển sinh</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://daotao.uneti.edu.vn/News.aspx?MenuID=394">Thông báo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://daotao.uneti.edu.vn/PageContent.aspx?MenuID=423">Giờ
+                                    học</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://daotao.uneti.edu.vn/PageContent.aspx?MenuID=436">Hướng
+                                    dẫn dành cho sinh viên</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+    </header>
+
+    <div class="wrapper container-fluid">
+        <div class="row" style="margin-top: 10px; ">
+            <div class="col-9" style=" margin-top: 10px; ">
+                <div class="head" style="background-color: #0098DB; color:#0098DB; width: 840px;">
+                    <h3>THÔNG BÁO</h3>
+                </div>
+                <p align="center">BIỂU MẪU DÙNG CHO SV THAM KHẢO</p>
+                <pre>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/DonChuyenDDHocTap.doc">1.  Đơn xin chuyển địa điểm học tập (xem chi tiết và download)</a>   
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/XinThoiHoc_EDIT.doc">2.  Đơn xin thôi học rút hồ sơ (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/DonXinbaoluuKQHT_edit.doc">3.  Đơn xin bảo lưu kết quả học tập (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_TroLaiHocTap.doc">4.  Đơn xin trở lại tiếp tục học tập (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_XinPhepNghiHoc.doc">5.  Đơn xin phép nghỉ học (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/XN_DoiTuongChinhSach.doc">6.  Xác nhận đối tượng chính sách (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/XacNhan_VayVon.doc">7.  Xác nhận vay vốn SV (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/GiayCamKetTraNo.doc">8.  Giấy cam kết trả nợ SV (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/XN_HocTaiTruong.doc">9.  Xác nhận đang học tại trường (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/XN_CapLaiTheBHYT.doc">10. Xác nhận cấp lại thẻ Bảo hiểm y tế (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/PhongCTHSSV/BCT&%C4%90NXT%20h%E1%BB%87%20%C4%90H.doc">11. Báo cáo tháng và đề nghị xóa tên hệ Đại học  (Xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/PhongCTHSSV/BCT&%C4%90NXT%20h%E1%BB%87%20C%C4%90.doc">12. Báo cáo tháng và đề nghị xóa tên hệ Cao đẳng (Xem chi tiết và download)</a>
+    <a href="http://daotao.uneti.edu.vn/images/HSSV/bieumau/7.Bien_ban_sinh_hoat_lop.doc">13. Biên bản sinh hoạt lớp (Xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/PhieuDG_DiemRL.doc">14. Phiếu đánh giá điểm rèn luyện (Xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/BBHopLop_DGDRL.doc">15. Biên bản họp lớp về đánh giá điểm rèn luyện (Xem chi tiết và download)</a>
+    <a href="http://daotao.uneti.edu.vn/images/HSSV/bieumau/14.PhieuXN_cac_lop.DOC">16. Phiếu xác nhận hoàn thành công việc (Xem chi tiết và download) </a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/BangTHDiemRL.doc">17. Tổng hợp điểm rèn luyện (Xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Mau13_Tong_hop_RL_ca_nam_toan_khoa.doc">18. Tổng hợp rèn luyện cả năm toàn khóa (Xem chi tiết và dowload)</a>
+    <a href="http://daotao.uneti.edu.vn/images/HSSV/bieumau/12.TrichngangngoaitruSV.xls">19. Trích ngang ngoại trú (Xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_XetCNTotNghiep(1).doc">20. Đơn xin xét công nhận tốt nghiệp và Giấy xác nhận nhân sự(xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_CapBangDiemTN.doc">21. Đơn xin cấp bảng điểm tốt nghiệp (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_CapBanSaoVBCC.doc">22. Đơn xin cấp bảng sao văn bằng, chứng chỉ từ sổ gốc (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_SuaSaiVB.doc">23. Đơn sửa sai trong văn bằng tốt nghiệp (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_XacNhanGiayToLienDT.doc">24. Đơn đề nghị xác nhận các giấy tờ liên quan đến quá trình học tập (xem chi tiết và download)</a>
+    <a href="https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/GiayUyQuyen.doc">25. Giấy ủy quyền lấy các giấy tờ liên quan đến quá trình học tập tại trường (xem chi tiết và download)</a>
+    <a href=https://daotao.uneti.edu.vn/Resource/Upload/file/MauBieuSV2020/Don_XinGiayGioiThieuDK_xeMay.doc"">26. Đơn xin cấp Giấy giới thiệu để đăng ký xe máy (xem chi tiết và download)</a>
+                </pre>
+            </div>
+            <div class="col-3" style="border :1px solid white;">
+                <div class="body">
+                    <div style=" background: #0098DB; color: rgb(243, 240, 233); margin-bottom: 10px;">
+                        <h3>ĐĂNG NHẬP</h3>
+                    </div>
+                    <div class="healdy">
+                        <div style="padding-left: 10px;">
+                            <label>Mã sinh viên:</label>
+                            <input type="text" placeholder="Nhập tên tài khoản" /><br> <br>
+                            <label>Mật khẩu:</label><br>
+                            <input type="password" placeholder="Nhập mật khẩu" />
+                        </div>
+                        <div style="padding-left: 10px;">
+                            <input type="radio" id="male" name="gender" value="male">
+                            <label for="male">Sinh viên đang học</label><br>
+                            <input type="radio" id="female" name="gender" value="female">
+                            <label for="female">Sinh viên tốt nghiệp</label><br>
+                        </div>
+                        <div style="padding-left: 10px;">
+                            <label>Mã bảo vệ:</label>
+                            <input type="text" placeholder="Nhập mã bảo vệ"> <img src="images/MTIvMDgvMjAyMCAyMzozMzo0NQ==.gif" alt="">
+                            <img src="images/Refresh.gif" alt="">
+
+                        </div>
+                        <div style="padding-left: 10px;">
+                            <br>
+                            <button type="Đăng nhập" class="btn btn-warning">Đăng nhập</button>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="body">
+                    <div class="sclot" style="background-color: #0098DB; color: rgb(243, 240, 233); font-size: 15px;">
+                        <h3>THÔNG BÁO</h3>
+                    </div>
+                    <div class="vertical-menu">
+                        <ul style="list-style: none;">
+                            <li>
+                                <a href="http://daotao.uneti.edu.vn/NewsDetail.aspx?NewsID=817">
+                                    <img src="images/arrow-2.png" alt="Thông báo v/v tiếp tục thực hiện các biện pháp phòng chống dịch Covid-19 từ ngày 23/4/2020 đến ngày 10/5/2020">
+                                    Thông báo v/v tiếp tục thực hiện các biện pháp phò...</a>
+                                <hr class="dashed">
+                            </li>
+
+                            <li>
+                                <a href="http://daotao.uneti.edu.vn/NewsDetail.aspx?NewsID=800">
+                                    <img src="images/arrow-2.png" alt="v/v tiếp tục thực hiện các biện pháp phòng chống dịch Covid-19 từ ngày 23/4/2020 đến ngày 10/5/2020">
+                                    Hướng dẫn SV thực hiện nội dung liên quan tới yêu ...</a>
+                                <hr class="dashed">
+
+                            <li>
+                                <a href="http://daotao.uneti.edu.vn/NewsDetail.aspx?NewsID=799">
+                                    <img src="images/arrow-2.png" alt="Hướng dẫn SV thực hiện nội dung liên quan tới yêu cầu về tài khoản Email">
+                                    Hướng dẫn SV thực hiện nội dung liên quan tới yêu ...</a>
+                                <hr class="dashed">
+                            </li>
+                            <a href="http://daotao.uneti.edu.vn/NewsDetail.aspx?NewsID=724">
+                                <img src="images/arrow-2.png" alt="Quyết định v/v miễn học, miễm thi và tính điểm các học phần Anh văn trong chương trình đào tạo cho sinh viên có chứng chỉ Tiếng Anh quốc tế">
+                                Quyết định v/v miễn học, miễm thi và tính điểm các...</a>
+                            <hr class="dashed">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="body">
+                    <div class="sclot" style="background-color: #0098DB; color: rgb(243, 240, 233); font-size: 13px;">
+                        <h3>DÀNH CHO PHỤ HUYNH</h3>
+                    </div>
+                    <div>
+                        <ul style="list-style: none;">
+                            <li>
+                                <a href="http://daotao.uneti.edu.vn/TraCuuThongTin.aspx">
+                                    <img src="images/arrow-2.png" alt="Tra cứu thông tin">
+                                    Tra cứu thông tin</a>
+                                <hr class="dashed">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="body">
+                    <div class="sclot" style="background-color: #0098DB; color: rgb(243, 240, 233);">
+                        <h3>LIÊN KẾT</h3>
+                    </div>
+                    <div class="vertical-menu">
+                        <ul style="list-style: none;">
+                            <li>
+                                <a href="http://iuh.edu.vn/">
+                                    <img src="images/arrow-2.png" alt="ĐH Công Nghiệp TP.HCM"> ĐH Công Nghiệp TP.HCM</a>
+                                <hr class="dashed">
+                            </li>
+                            <li>
+                                <a href="http://ascvn.com.vn/">
+                                    <img src="images/arrow-2.png" alt="Công ty TNHH Tiến bộ Sài Gòn"> Công ty TNHH Tiến
+                                    bộ Sài Gòn</a>
+                                <hr class="dashed">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="body">
+                    <div class="scolt" style="background-color: #0098DB; color: rgb(243, 240, 233);">
+                        <h3>THỐNG KÊ</h3>
+                    </div>
+                    <div>
+                        <img src="images/khach.png" alt=""> Đang online: 266</img>
+                        <br>
+                        <img src="images/homnay.png" alt=""> Hôm nay:9159</img>
+                        <br>
+                        <img src="images/homqua.png" alt=""> Hôm qua: 21369</img>
+                        <br>
+                        <img src="images/tongcong.png" alt=""> Tổng cộng: 57465421</img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <footer>
+        <div class="wrapper footerr">
+            <p>Copyright © 2013 Trường Đại học Kinh tế Kỹ thuật Công nghiệp - Phòng Đào Tạo</p>
+            <p>Địa chỉ: 456-Minh Khai, Hà Nội - Fax:(04)8623938</p>
+            <p>Điện thoại: (04)8621504 - Email: uneti@vnn.vn</p>
+            <p>Thiết kế bởi EPMT</p>
+        </div>
+    </footer>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
